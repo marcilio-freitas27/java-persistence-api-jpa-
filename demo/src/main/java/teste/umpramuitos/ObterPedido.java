f5caae8f94302961b75dd2429cb9fd22da6db974
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-03-07 08:05:34 -0300

    Correções de um para muitos - segundo exemplo

--- demo/src/main/java/teste/umpramuitos/ObterPedido.java
+++ demo/src/main/java/teste/umpramuitos/ObterPedido.java
@@ -10,11 +10,13 @@ public class ObterPedido {
         
         DAO<Pedido> dao = new DAO<>(Pedido.class);
 
-        Pedido pedido = dao.obterPorId(1);
+        Pedido pedido = dao.obterPorId(1L);
 
         for (ItemPedido item : pedido.getItens()) {
             System.out.println(item.getQuantidade());
             System.out.println(item.getProduto().getNome());
         }
+
+        dao.fechar();
     }
 }
